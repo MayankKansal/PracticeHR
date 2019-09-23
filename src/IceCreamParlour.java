715dd4b52374d8af220d35ee9b185d1d58aa57b4
mayank <mayank93kansal@gmail.com>
2019-09-23 20:18:33 +0530

    from acer

--- src/IceCreamParlour.java
+++ src/IceCreamParlour.java
@@ -19,8 +19,8 @@ public class IceCreamParlour {
 			}
 
 		}
-		System.out.println(first);
-		System.out.println(second);
+	//	System.out.println(first);
+	//	System.out.println(second);
 		return result;
 
 	}
