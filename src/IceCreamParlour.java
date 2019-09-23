ad5399764351052d95b9cb8c374e8c9a71ea2640
martin <martin@123789.org>
2019-09-23 15:49:22 +0530

    changes in ice loop

--- src/IceCreamParlour.java
+++ src/IceCreamParlour.java
@@ -7,7 +7,7 @@ public class IceCreamParlour {
 		int second = 0;
 		
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 1; j < arr.length; j++) {
+			for (int j = i+1; j < arr.length; j++) {
 
 				if ((arr[i] + arr[j]) == m) {
 
@@ -31,7 +31,7 @@ public class IceCreamParlour {
 		// TODO Auto-generated method stub
 
 		IceCreamParlour res = new IceCreamParlour();
-		System.out.println(Arrays.toString(res.iceCreamParlour(5, new int[] { 2, 3, 4 })));
+		System.out.println(Arrays.toString(res.iceCreamParlour(4, new int[] { 2,2, 3, 4 })));
 	//.out.println(Arrays.to);
 
 	}
