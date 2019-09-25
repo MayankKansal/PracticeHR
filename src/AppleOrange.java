3de0904e3fac5a26ad41af5f8c5fda9709b5861c
martin <martin@123789.org>
2019-09-25 14:59:40 +0530

    vector to array Martin

--- src/AppleOrange.java
+++ src/AppleOrange.java
@@ -3,7 +3,7 @@ import java.util.Vector;
 public class AppleOrange {
 
 	void countApplesAndOranges(int s, int t, int a, int b,
-			Vector<Integer> apples, Vector<Integer> oranges) {
+			int[] apples, int[] oranges) {
 
 		int Sam_start_point = 0;
 		int Sam_end_point = 0;
@@ -12,15 +12,15 @@ public class AppleOrange {
 		int ornagePoint = 0;
 		int appleAthouse = 0;
 		int ornageAthouse = 0;
-		for (int i = 0; i < apples.size(); i++) {
-			if (applePoint + apples.get(i) == Sam_start_point) {
+		for (int i = 0; i < apples.length; i++) {
+			if (applePoint + apples[i] == Sam_start_point) {
 				appleAthouse++;
 			}
 
 		}
 
-		for (int j = 0; j < oranges.size(); j++) {
-			if (applePoint + apples.get(j) == Sam_end_point) {
+		for (int j = 0; j < oranges.length; j++) {
+			if (applePoint + apples[j] == Sam_end_point) {
 				ornageAthouse++;
 			}
 
