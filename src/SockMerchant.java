ea16733927f6a7788581a01d6300b98f530cdca7
martin <martin@123789.org>
2019-09-25 14:51:40 +0530

    push from martin

--- src/SockMerchant.java
+++ src/SockMerchant.java
@@ -1,16 +1,17 @@
-import java.util.ArrayList;
-import java.util.Vector;
+
+import java.util.Arrays;
+
 
 public class SockMerchant {
 
-	int sockMerchant(int n, Vector<Integer> ar) {
+	int sockMerchant(int n, int [] ar) {
 
 		int pair = 0;
-		for (int i = 0; i < ar.size(); i++) {
-			for(int j=i+1; j<ar.size(); j++){
-				if (ar.get(i)==ar.get(j)) {
+		for (int i = 0; i < ar.length; i++) {
+			for(int j=i+1; j<ar.length; j++){
+				if (ar[i]==ar[j]) {
 					pair++;
-					ar.remove(j);
+					ArrayUtils.removeElement(ar, j);
 					break;
 				}
 			}
@@ -25,16 +26,7 @@ public class SockMerchant {
 		// TODO Auto-generated method stub
 
 		SockMerchant sm =new SockMerchant();
-		Vector<Integer> al = new Vector<Integer>();
-		
-		al.add(1);
-		al.add(2);
-		al.add(3);
-		al.add(4);
-		al.add(2);
-		al.add(2);
-		al.add(3);
-		System.out.println(sm.sockMerchant(7, al));
+		sm.sockMerchant(7, new int[]{1,2,3,1,2,3,3});
 		
 		
 	}
