d4a62bccb36580045e100d7dc8ad83822f364dee
mayank <mayank93kansal@gmail.com>
2019-10-09 01:08:42 +0530

    acer cake

--- src/BirthdayCandle.java
+++ src/BirthdayCandle.java
@@ -6,10 +6,14 @@ public class BirthdayCandle {
 	static int birthdayCakeCandles(int[] ar) {
         int candle =1;
         Arrays.sort(ar);
-        for(int i=0; i<ar.length-1;i++){
+        System.out.println(Arrays.toString(ar));
+        for(int i=ar.length-1; i>=0;i--){
            
-            if(ar[i]==ar[i+1])
-            candle++;
+            if(ar[i]==ar[i-1]) {
+            	candle++;	
+            }
+            else
+            break;
            
            
         }
@@ -19,7 +23,7 @@ public class BirthdayCandle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(birthdayCakeCandles(new int[] {4,5,1,3}));
+		System.out.println(birthdayCakeCandles(new int[] {4,4,4,5,5,1,5,5,1,3}));
 	}
 
 }
