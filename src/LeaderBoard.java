0f300472d2d7fe05afb656fb5d139d8e2a5af7ec
mayank <mayank93kansal@gmail.com>
2019-10-08 20:55:53 +0530

    from acer

--- src/LeaderBoard.java
+++ src/LeaderBoard.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
 
 public class LeaderBoard {
 
+	 static int[] climbingLeaderboard(int[] scores, int[] alice) {
+		
+		 ArrayList<Integer> al = new ArrayList<Integer>();
+		 
+		 int size = scores.length +  alice.length;
+		 int board[] = new int[size];
+		 int rank=1;
+		 
+		 for(int i=0; i<scores.length;i++) {
+			 for (int j=0; j<alice.length;j++) {
+				 
+			 }
+			 
+		 }
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 return alice;
+
+
+	    }
+
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/LexiStringCompare.java
+++ src/LexiStringCompare.java
@@ -5,35 +5,39 @@ import java.util.Collections;
 public class LexiStringCompare {
 	
 	public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
+        String smallest = s.substring(0,k);
+        String largest = s.substring(0,k);
         
-        ArrayList<String> al = new ArrayList<String>();
-        String ar[] = new String[s.length()];
-       
         
-        try {
-        	 for(int i=0; i<s.length();i++) {
-             	ar [i] =s.substring(i, k);
-             	System.out.println(ar[i]);
-             	k++;
-             }
-        	 Arrays.sort( ar ); 	
-        }
-        catch(Exception e) {
-        	
-        }
-        int size = ar.length;
-        System.out.println(Arrays.toString(ar));
         
-        System.out.println(Arrays.toString(ar));
-        smallest =ar[0];
-        largest=ar[size-1];
         
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
         
+        
+        
+        
+        
+        
+        
+/*	ArrayList<String> al = new ArrayList<String>();
+		String ar[] = new String[s.length()];
+
+		try {
+			for (int i = 0; i < s.length(); i++) {
+				ar[i] = s.substring(i, k);
+				System.out.println(ar[i]);
+				k++;
+			}
+			Arrays.sort(ar);
+		} catch (Exception e) {
+
+		}
+		int size = ar.length;
+		System.out.println(Arrays.toString(ar));
+
+		System.out.println(Arrays.toString(ar));
+		smallest = ar[0];
+		largest = ar[size - 1];
+*/
         return smallest + "\n" + largest;
     }
 
