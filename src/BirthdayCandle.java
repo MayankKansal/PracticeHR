e87cec05b76555f5788a495371e13887baabbb6b
martin <martin@123789.org>
2019-10-09 12:45:41 +0530

    candle solved

--- src/BirthdayCandle.java
+++ src/BirthdayCandle.java
@@ -8,22 +8,24 @@ public class BirthdayCandle {
         Arrays.sort(ar);
         System.out.println(Arrays.toString(ar));
         for(int i=ar.length-1; i>=0;i--){
-           
-            if(ar[i]==ar[i-1]) {
-            	candle++;	
-            }
-            else
-            break;
-           
-           
+          try{
+        	   if(ar[i]==ar[i-1]) {
+               	candle++;	
+               }
+        	   else
+                   break; 
+           }
+          catch(Exception e){
+        	  
+          }
+                 
         }
     return candle;
     }
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(birthdayCakeCandles(new int[] {4,4,4,5,5,1,5,5,1,3}));
+		System.out.println(birthdayCakeCandles(new int[] {5,5,5,5,5,5,5,5}));
 	}
 
 }
