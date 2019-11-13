b3eb03a9023786fe453f3c4eee926dd74bd8a396
martin <martin@123789.org>
2019-11-13 16:18:09 +0530

    from matrin 3 new 1 unsloved

--- src/CeasarCipher.java
+++ src/CeasarCipher.java
@@ -1,6 +1,20 @@
 
 public class CeasarCipher {
 
+	static String caesarCipher(String s, int k) {
+		
+		char a;
+		for(int i=0; i<s.length(); i++){
+			a= s.charAt(i);
+		}
+		
+		
+		
+		
+		return s;
+
+
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
