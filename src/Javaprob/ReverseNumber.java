1aec0e92514f84a7fbfc4d2b771b0041c44766be
martin <martin@123789.org>
2019-11-20 12:09:32 +0530

    Commented code

---
+++ src/Javaprob/ReverseNumber.java
@@ -0,0 +1,27 @@
+package Javaprob;
+
+public class ReverseNumber {
+
+    static int  reverseNumber(int n){
+        int  newNumber=0;
+     while(n>0) {
+          int div  =n%10;
+          System.out.println(div);
+          newNumber=newNumber*10+div;
+          n=n/10;
+      }
+
+
+        return newNumber;
+    }
+
+
+
+
+    public static void main(String [] args){
+
+        System.out.println(reverseNumber(00547));
+
+    }
+
+}
