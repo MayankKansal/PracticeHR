793e071aa4040a4c8a2f0cafb0c90da4d3f4bd16
martin <martin@123789.org>
2019-12-06 14:56:00 +0530

    from martin staircse

--- src/Staircase.java
+++ src/Staircase.java
@@ -2,21 +2,24 @@
 
 public class Staircase {
 	
-	public void staircase(int n) {
+	static void staircase(int n) {
 
-		for (int i = 0; i <= n; i++) {
-			for (int j = i; j < i - 1; j++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = n; j > i + 1; j--) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <i+1; k++) {
 				System.out.print("#");
 			}
-			System.out.print(" ");
+			System.out.println();
 		}
+		//System.out.println(" ");
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Staircase st= new Staircase();
-		st.staircase(4);
+		staircase(10);
 	}
 
 }
