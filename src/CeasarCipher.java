8c8e6f90929a23ca537524c57ff0949d1740bb76
martin <martin@123789.org>
2019-12-06 18:14:41 +0530

    from martin cipher text

--- src/CeasarCipher.java
+++ src/CeasarCipher.java
@@ -1,23 +1,42 @@
-
 public class CeasarCipher {
 
 	static String caesarCipher(String s, int k) {
-		
+
 		char a;
-		for(int i=0; i<s.length(); i++){
-			a= s.charAt(i);
+		String c = "";
+
+		for (int i = 0; i < s.length(); i++) {
+			a = s.charAt(i);
+			int num = a;
+			
+			if (a != '-') {
+				if (num<= 122 && num>=97) {
+					//num = num - 26 + k;
+					num = num + k;
+					if(num>122)
+					num=96+num-122;
+				} else if (num <= 90 && num>=65) {
+					num = num + k;
+					if(num>90)
+					num=64+num-90;
+				}
+
+				a = (char) num;
+
+				c = c + a;
+			} else
+				c = c + '-';
+
 		}
-		
-		
-		
-		
-		return s;
 
+		return c;
 
-    }
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(caesarCipher("www.abc.xy", 87));
 	}
 
 }
