ff5a49d213a89f7dbe3a7cd085646014a1c5f1dc
martin <martin@123789.org>
2019-12-30 14:42:19 +0530

    from martin

--- src/SockMerchant.java
+++ src/SockMerchant.java
@@ -1,19 +1,23 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class SockMerchant {
 
-	int sockMerchant(int n, int [] ar) {
+	int sockMerchant(int n, Integer [] ar) {
 
 		int pair = 0;
+		/*Integer[] ints = new Integer[] {1,2,3,4,5};
+		List<Integer> list = Arrays.asList(ints);
+		*/
+		List<Integer> al= (Arrays.asList(ar));
 		
-		ArrayList<Integer> al= new ArrayList<Integer>();
-		for (int i = 0; i < ar.length; i++) {
+		/*for (int i = 0; i < ar.length; i++) {
 			
 				al.add(ar[i]);
-			}
+			}*/
 		
 		
 		for (int i = 0; i < al.size(); i++) {
@@ -34,7 +38,7 @@ public class SockMerchant {
 		// TODO Auto-generated method stub
 
 		SockMerchant sm =new SockMerchant();
-		System.out.println(sm.sockMerchant(7, new int[]{1,2,3,1,2,3,3}));
+		System.out.println(sm.sockMerchant(7, new Integer[]{1,2,3,1,2,3,3}));
 		
 		
 	}
